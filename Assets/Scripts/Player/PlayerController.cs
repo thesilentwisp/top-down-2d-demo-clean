67bf36027245832e76deb528315875ef3fa30714
thesilentwisp <gabesolomon94@gmail.com>
2026-03-21 07:57:35 -0400

    Initial Unity project commit

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private InputActionProperty moveAction;
+
+    private Rigidbody2D rb;
+    private InputAction defaultMoveAction;
+    private Vector2 movementInput;
+
+    public float MoveSpeed
+    {
+        get => moveSpeed;
+        set => moveSpeed = Mathf.Max(0f, value);
+    }
+
+    private InputAction ActiveMoveAction => moveAction.action ?? defaultMoveAction;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        if (moveAction.action == null)
+        {
+            defaultMoveAction = new InputAction(name: "Move");
+            defaultMoveAction.AddCompositeBinding("2DVector")
+                .With("Up", "<Keyboard>/w")
+                .With("Down", "<Keyboard>/s")
+                .With("Left", "<Keyboard>/a")
+                .With("Right", "<Keyboard>/d");
+            defaultMoveAction.AddCompositeBinding("2DVector")
+                .With("Up", "<Keyboard>/upArrow")
+                .With("Down", "<Keyboard>/downArrow")
+                .With("Left", "<Keyboard>/leftArrow")
+                .With("Right", "<Keyboard>/rightArrow");
+            defaultMoveAction.AddBinding("<Gamepad>/leftStick");
+        }
+    }
+
+    private void OnEnable()
+    {
+        ActiveMoveAction?.Enable();
+    }
+
+    private void OnDisable()
+    {
+        ActiveMoveAction?.Disable();
+    }
+
+    private void OnDestroy()
+    {
+        defaultMoveAction?.Dispose();
+    }
+
+    private void Update()
+    {
+        if (ActiveMoveAction == null)
+        {
+            movementInput = Vector2.zero;
+            return;
+        }
+
+        movementInput = ActiveMoveAction.ReadValue<Vector2>();
+        movementInput = Vector2.ClampMagnitude(movementInput, 1f);
+    }
+
+    private void FixedUpdate()
+    {
+#if UNITY_6000_0_OR_NEWER
+        rb.linearVelocity = movementInput * moveSpeed;
+#else
+        rb.velocity = movementInput * moveSpeed;
+#endif
+    }
+}
